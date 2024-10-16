7d67528b509385b1b2dc7d3f27529beac4d5924b
Suraj Bhatt <surajbhatt54321@gmail.com>
2024-10-16 19:06:15 +0530

    implement basic navbar and homepage component

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,21 @@
+
+
+const NavBar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/about">About</a>
+                </li>
+                <li>
+                    <a href="/articles">Articles</a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
