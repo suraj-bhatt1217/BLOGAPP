44eb93bbaeabf309af94a55780f7276dc248864f
Suraj Bhatt <surajbhatt54321@gmail.com>
2024-10-16 21:11:43 +0530

    connect fe with be using axios

--- frontend/src/pages/ArticlePage/ArticlePage.jsx
+++ frontend/src/pages/ArticlePage/ArticlePage.jsx
@@ -1,20 +1,47 @@
 import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import articles from '../../data/article-content';
 
 // pages
 import NotFoundPage from '../NotFoundPage/NotFoundPage';
 
+import axios from 'axios';
+
+
+
 const ArticlePage = () => {
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
     const { articleId } = useParams();
+
+    useEffect(() => {
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/articles/${articleId}`);
+            const newArticleInfo = response.data;
+            setArticleInfo(newArticleInfo);
+        }
+
+        loadArticleInfo();
+    }, []);
+
     const article = articles.find(article => article.name === articleId);
+
+    const addUpvote = async () => {
+        const response = await axios.put(`/api/articles/${articleId}/upvote`);
+        const updatedArticle = response.data;
+        setArticleInfo(updatedArticle);
+    }
 
     if (!article) {
         return <NotFoundPage />
-    } 
+    }
 
     return (
         <>
         <h1>{article.title}</h1>
+        <div className="upvotes-section">
+            <button onClick={addUpvote}>Upvote</button>
+            <p>This article has {articleInfo.upvotes} upvote(s)</p>
+        </div>
         {article.content.map((paragraph, i) => (
             <p key={i}>{paragraph}</p>
         ))}
